96bf968e35b85040c9b0e3133944a0e0e6d027d7
Aleksey <Aleksey@DESKTOP-MSI>
2023-09-29 16:13:41 +0500

    asyncio and _thread in main.py

--- main.py
+++ main.py
@@ -8,23 +8,39 @@ import temperature_module
 import mqtt_module
 import settings_module
 
+import _thread
+import asyncio
+import sys
+
 from mqtt_module import HaMqttPublisher
 
-settings = settings_module.load_settings()
+def sync_temperature_thread():
+    settings = settings_module.load_settings()
 
-wifi_network_module.connect_wifi(settings.wifi_ssid, settings.wifi_password)
+    wifi_network_module.connect_wifi(settings.wifi_ssid, settings.wifi_password)
 
-time_module.set_ntp_time()
+    time_module.set_ntp_time()
 
-led_module.led_on()
+    led_module.led_on()
 
-ha_client = HaMqttPublisher(settings.mqtt_broker)
+    ha_client = HaMqttPublisher(settings.mqtt_broker)
 
-ha_client.publish_config()
+    ha_client.publish_config()
 
-while True:
-    temperature_module.log_temperature()
-    temperature = temperature_module.get_temperature()
-    ha_client.publish_temperature(temperature)
-    utime.sleep(300)
-    led_module.led_toggle()
+    while True:
+        temperature_module.log_temperature()
+        temperature = temperature_module.get_temperature()
+        ha_client.publish_temperature(temperature)
+        utime.sleep(300)
+        led_module.led_toggle()
+
+async def main():
+    _thread.start_new_thread(sync_temperature_thread, ())
+    while True:
+        await asyncio.sleep(10)
+        print("looping")
+
+    
+    
+asyncio.run(main())
+
